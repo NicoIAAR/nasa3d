d090c7436a6ec8e46c39fdf53f43a2c923d7db0f
nico-iaar <NicolasHernanAguero@live.com>
2017-04-29 13:04:09 -0300

    add all

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+/*celestrak
+var Satcat = require( 'Satcat' );
+var parser = new Satcat.Parser()
+  .on( 'data', function( satellite ) {
+    console.log( satellite )
+    console.log( '' )
+  })
+
+router.get( 'http://www.celestrak.com/pub/satcat.txt', function( response ) {
+  if( response.statusCode === 200 ) {
+    response.pipe( parser )
+  } else {
+    console.log( 'HTTP', response.statusCode )
+    process.exit( 1 )
+  }
+})
+*/
